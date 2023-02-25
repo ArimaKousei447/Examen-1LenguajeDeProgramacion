d7aa3b3c3707d0905319459902fc6d6a05b2c4fc
ArimaKousei447 <124010225+ArimaKousei447@users.noreply.github.com>
2023-02-25 00:25:20 -0600

    Inicio examen
    Inicio de ejercicio 1

---
+++ Examen1-LenguajeDeProgramacion3/Examen1-LenguajeDeProgramacion3/Ejercicio-1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen1_LenguajeDeProgramacion3
+{
+    public partial class Ejercicio_1 : Form
+    {
+        public Ejercicio_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
